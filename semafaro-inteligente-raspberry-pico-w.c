09d4890822bcac1dc4ba4466b21ffc274a503899
lima_rich <richard.esclima@gmail.com>
2025-04-30 22:06:09 -0300

    feat: adiciona controle de mudança de modo via interrupçao

--- semafaro-inteligente-raspberry-pico-w.c
+++ semafaro-inteligente-raspberry-pico-w.c
@@ -4,11 +4,30 @@
 #include "task.h"
 #include "FreeRTOSConfig.h"
 
-#define ledR 13 // pino do led vermelho
-#define ledG 11 // pino do led verde
-#define signalDelay 2000
+#define ledR 13          // pino do led vermelho
+#define ledG 11          // pino do led verde
+#define signalDelay 2000 // tempo que o led fica aceso
+#define BUTTON_A 5
+#define DEBOUNCE_MS 200 // intervalo minimo de 200ms para o debounce
 
-bool night_mode = true;
+volatile int last_interrupt_a = 0; // variavel para controlar o debounce
+volatile bool night_mode = true;
+
+// gerenciador de interrupcoes
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    // variavel para o controle do debounce
+    uint current_time = to_ms_since_boot(get_absolute_time());
+
+    if (gpio == BUTTON_A)
+    {
+        if (current_time - last_interrupt_a > DEBOUNCE_MS)
+        {
+            last_interrupt_a = current_time;
+            night_mode = !night_mode; // muda do modo normal para noturno e vice-versa
+        }
+    }
+}
 
 void vSemafaroNormalTask(void *pvParameters)
 {
@@ -72,6 +91,14 @@ void vSemafaroNoturnoTask(void *pvParameters)
 int main()
 {
     stdio_init_all();
+
+    // configuração do butão A
+    gpio_init(BUTTON_A);
+    gpio_set_dir(BUTTON_A, GPIO_IN);
+    gpio_pull_up(BUTTON_A);
+    // habilita interrupção com o botão
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
     xTaskCreate(vSemafaroNormalTask, "Semafaro Normal", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
     xTaskCreate(vSemafaroNoturnoTask, "Semafaro Noturno", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
     vTaskStartScheduler();
